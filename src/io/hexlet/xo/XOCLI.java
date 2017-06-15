c294faeb29956c63bd8bc62e8a1d5b8a5182fa64
Blicker777 <lordid@rambler.ru>
2017-06-15 15:56:26 +0300

    Доработан класс XOCLI. Теперь в консоль вводятся имена игроков

--- src/io/hexlet/xo/XOCLI.java
+++ src/io/hexlet/xo/XOCLI.java
@@ -7,22 +7,32 @@ import io.hexlet.xo.model.Game;
 import io.hexlet.xo.model.Player;
 import io.hexlet.xo.view.ConsoleView;
 
+import java.util.Scanner;
+
 public class XOCLI {
 
     public static void main(String[] args) {
 
-        String name1 = "Igor";
-        String name2 = "Sveta";
+        String gameName = "XO";
+        System.out.format("Game name: %s\n", gameName);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write name one:");
+        String name1 = scanner.nextLine();
+        System.out.println("Write name two:");
+        String name2 = scanner.nextLine();
 
         final Player[] players = new Player[2];
         players[0] = new Player(name1, Figure.X);
         players[1] = new Player(name2, Figure.O);
 
-        final Game game = new Game(players, new Field(), "XO");
+        final Game game = new Game(players, new Field(), gameName);
+        System.out.println("Player: " + players[0].getName() + " Figure: " + players[0].getFigure());
+        System.out.println("Player: " + players[1].getName() + " Figure: " + players[1].getFigure());
 
         ConsoleView consoleView = new ConsoleView();
         consoleView.show(game);
-        while (consoleView.move(game)) {
+        while (consoleView.move(game, players)) {
             consoleView.show(game);
         }
 
